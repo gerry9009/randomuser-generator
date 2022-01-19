e790a0eb057381900012889e073beb1c30f62bc8
Buglyó Gergő <buglyogergo@gmail.com>
2022-01-19 21:27:55 +0100

    Fixed location validation bug. Changed UserFilter structure and added feedback message function

--- src/components/UserFilter/UserFilter.jsx
+++ src/components/UserFilter/UserFilter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NATIONALITIES from "../constants/nationalities.js";
 import getRandomKey from "../constants/getRandomKey.js";
 import {
@@ -7,11 +7,16 @@ import {
   Container,
   Navbar,
   Offcanvas,
-  Alert,
+  ToastContainer,
+  Toast,
 } from "react-bootstrap";
 
 export default function UserFilter(props) {
+  const [addedUserMessageShow, setAddedUserMessageShow] = useState(false);
+  const [addedUsersMessageShow, setAddedUsersMessageShow] = useState(false);
+  const [clearMessageShow, setClearMessageShow] = useState(false);
   const addNewElement = (e) => {
+    setAddedUserMessageShow(true);
     e.preventDefault();
     const nationality = document.querySelector(
       ".js-form-nationality-one"
@@ -21,6 +26,7 @@ export default function UserFilter(props) {
   };
 
   const addNewListOfElements = (e) => {
+    setAddedUsersMessageShow(true);
     e.preventDefault();
     const length = document.querySelector(".js-form-length").value;
     const nationality = document.querySelector(".js-form-nationality").value;
@@ -31,6 +37,7 @@ export default function UserFilter(props) {
   const deleteListOfElements = (e) => {
     e.preventDefault();
     props.handleDeleteList();
+    setClearMessageShow(true);
   };
 
   const getListOfNationalities = Object.values(NATIONALITIES);
@@ -40,93 +47,181 @@ export default function UserFilter(props) {
     }
   );
 
+  const listGroupNationality = (formClassName) => {
+    return (
+      <Form.Group className="mb-3">
+        <Form.Label>Nationality</Form.Label>
+        <Form.Select size="sm" className={formClassName}>
+          <option>Any</option>
+          {optionsListOfNationalities}
+        </Form.Select>
+      </Form.Group>
+    );
+  };
+
+  const listGroupLength = (
+    <Form.Group className="mb-3">
+      <Form.Label>Length of list</Form.Label>
+      <Form.Select size="sm" className="js-form-length">
+        <option>5</option>
+        <option>10</option>
+        <option>15</option>
+        <option>20</option>
+        <option>30</option>
+        <option>40</option>
+        <option>50</option>
+      </Form.Select>
+    </Form.Group>
+  );
+
+  const listGroupGender = (formClassName) => {
+    return (
+      <Form.Group className="mb-3">
+        <Form.Label>Gender</Form.Label>
+        <Form.Select size="sm" className={formClassName}>
+          <option>any</option>
+          <option>male</option>
+          <option>female</option>
+        </Form.Select>
+      </Form.Group>
+    );
+  };
+
+  const clearMessage = (
+    <ToastContainer className="p-3 zindex" bg="secondary">
+      <Toast
+        onClose={() => setClearMessageShow(false)}
+        show={clearMessageShow}
+        delay={3000}
+        autohide
+        bg="secondary"
+      >
+        <Toast.Header>
+          <strong className="me-auto">Random User</strong>
+          <small>Now</small>
+        </Toast.Header>
+        <Toast.Body className="text-white">
+          You have successfully deleted the list of users.
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+
+  const addedUserMessage = (
+    <ToastContainer className="p-3 zindex" bg="secondary">
+      <Toast
+        onClose={() => setAddedUserMessageShow(false)}
+        show={addedUserMessageShow}
+        delay={3000}
+        autohide
+        bg="secondary"
+      >
+        <Toast.Header>
+          <strong className="me-auto">Random User</strong>
+          <small>Now</small>
+        </Toast.Header>
+        <Toast.Body className="text-white">
+          You have successfully added a user to the list.
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+
+  const addedUsersMessage = (
+    <ToastContainer className="p-3 zindex" bg="secondary">
+      <Toast
+        onClose={() => setAddedUsersMessageShow(false)}
+        show={addedUsersMessageShow}
+        delay={3000}
+        autohide
+        bg="secondary"
+      >
+        <Toast.Header>
+          <strong className="me-auto">Random User</strong>
+          <small>Now</small>
+        </Toast.Header>
+        <Toast.Body className="text-white">
+          You have successfully added new users to the list.
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+
   return (
-    <Navbar bg="dark" variant="dark" expand={false} className="sticky-top">
-      <Container fluid>
-        <Navbar.Brand href="#">Random User</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">Editor</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body className="">
-            <Form>
-              <Alert>Add new users to the list</Alert>
-              <fieldset>
-                <Form.Group className="mb-3">
-                  <Form.Label>Nationality</Form.Label>
-                  <Form.Select size="sm" className="js-form-nationality">
-                    <option>Any</option>
-                    {optionsListOfNationalities}
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label>Length of list</Form.Label>
-                  <Form.Select size="sm" className="js-form-length">
-                    <option>5</option>
-                    <option>10</option>
-                    <option>15</option>
-                    <option>20</option>
-                    <option>30</option>
-                    <option>40</option>
-                    <option>50</option>
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label>Gender</Form.Label>
-                  <Form.Select size="sm" className="js-form-gender">
-                    <option>any</option>
-                    <option>male</option>
-                    <option>female</option>
-                  </Form.Select>
-                </Form.Group>
+    <>
+      <Navbar bg="dark" variant="dark" expand={false} className="sticky-top">
+        <Container fluid>
+          <Navbar.Brand href="#">Random User</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                Editor
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="">
+              <Form className="bg-light bg-gradient mt-0">
+                <fieldset>
+                  <Form.Label column="lg" sm={12}>
+                    Add new users to the list
+                  </Form.Label>
+                  {listGroupNationality("js-form-nationality")}
+                  {listGroupLength}
+                  {listGroupGender("js-form-gender")}
+                  <div className="d-grid gap-2">
+                    <Button
+                      variant="secondary"
+                      type="submit"
+                      onClick={addNewListOfElements}
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </fieldset>
+              </Form>
+              <Form className="bg-light bg-gradient mt-3">
+                <fieldset>
+                  <Form.Label column="lg" sm={12}>
+                    Add only one user to the list
+                  </Form.Label>
+                  {listGroupNationality("js-form-nationality-one")}
+                  {listGroupGender("js-form-gender-one")}
+                  <div className="d-grid gap-2">
+                    <Button
+                      variant="secondary"
+                      type="submit"
+                      onClick={addNewElement}
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </fieldset>
+              </Form>
+              <Form className="bg-light bg-gradient mt-3">
+                <Form.Label column="lg" sm={12}>
+                  Remove all items from the list
+                </Form.Label>
                 <div className="d-grid gap-2">
                   <Button
+                    variant="secondary"
                     type="submit"
-                    size="lg"
-                    onClick={addNewListOfElements}
+                    onClick={deleteListOfElements}
                   >
-                    Submit
+                    Clear
                   </Button>
                 </div>
-              </fieldset>
-            </Form>
-            <Alert variant="secondary mt-4">Add new user to the the list</Alert>
-            <fieldset>
-              <Form.Group className="mb-3">
-                <Form.Label>Nationality</Form.Label>
-                <Form.Select size="sm" className="js-form-nationality-one">
-                  <option>Any</option>
-                  {optionsListOfNationalities}
-                </Form.Select>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Gender</Form.Label>
-                <Form.Select size="sm" className="js-form-gender-one">
-                  <option>any</option>
-                  <option>male</option>
-                  <option>female</option>
-                </Form.Select>
-              </Form.Group>
-              <div className="d-grid gap-2">
-                <Button variant="dark" onClick={addNewElement}>
-                  Add
-                </Button>
-              </div>
-            </fieldset>
-            <Alert variant="danger mt-5">Remove all items from the list</Alert>
-            <div className="d-grid gap-2">
-              <Button variant="danger" onClick={deleteListOfElements}>
-                Clear
-              </Button>
-            </div>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+      {addedUserMessage}
+      {addedUsersMessage}
+      {clearMessage}
+    </>
   );
 }
